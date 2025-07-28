a8400bdf1286161a8ca367ca5b32ab5db384e689
Ernest Beloy <49635535+Justcosa@users.noreply.github.com>
2025-07-28 23:03:47 +0800

    Updated startup.py with button for statsPage

--- pages/authentication/startup.py
+++ pages/authentication/startup.py
@@ -1,14 +1,11 @@
 import flet as ft
+from utils.colors import customTextcolor2
 
 class startup(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
 
         self.expand = True
-
-        page.window_width = 800
-        page.window_height = 600
-        page.update()
 
         def go_to_log_window(e):
             page.go("/logWindow")
@@ -19,23 +16,25 @@ class startup(ft.Container):
         def go_to_resources(e):
             page.go("/resources")
 
+        def go_to_stats(e):
+            page.go("/statsPage")
+
         self.content = ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,  # Center horizontally
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,  # Center vertically
             controls=[
                 ft.Container(
                     content=ft.Column(
-                        alignment=ft.MainAxisAlignment.CENTER,
-                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        alignment=ft.MainAxisAlignment.CENTER,  # Center vertically in column
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,  # Center horizontally in column
                         controls=[
-                            ft.Text("Welcome back!", color="black", size=30, weight=ft.FontWeight.BOLD),
+                            ft.Text("Welcome back!", color=customTextcolor2, size=30, weight=ft.FontWeight.BOLD),
                             ft.ElevatedButton("New Entry", on_click=go_to_log_window),
                             ft.ElevatedButton("History", on_click=go_to_history),
-                            ft.ElevatedButton("Resources", on_click=go_to_resources)
+                            ft.ElevatedButton("Resources", on_click=go_to_resources),
+                            ft.ElevatedButton("Statistics", on_click=go_to_stats)
                         ]
                     )
                 )
             ]
         )
-
-    #def route_change(self, route):
-        # Handle route changes here if needed
-        #pass
